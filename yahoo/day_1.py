4b71d34ea568e3475c5c769c5ef2a577068642f2
ming-666-ming <hongming666@outlook.com>
2022-06-28 09:04:14 +0800

    first

---
+++ yahoo/day_1.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+"""
+Name : day_1.py
+Author  : Mark
+Contect : hongming666@outlook.com
+Time    : 2022/6/23 21:28
+Desc:
+"""
+import requests
+from selenium import webdriver
+
+
+webdriver.PhantomJS()
+def main():
+    """
+    主函数
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
